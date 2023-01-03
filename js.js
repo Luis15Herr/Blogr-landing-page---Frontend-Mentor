6bd3ca1315b2a66ab2f22984006d8fd8f7f8c379
luis15herr <herrera05luis@gmail.com>
2023-01-02 21:05:37 -0400

    Nav 95%

--- js.js
+++ js.js
@@ -26,10 +26,18 @@ document.addEventListener("click", function (e) {
 
 let mobileTrigger = document.querySelector(".mobile__nav-trigger");
 let mobileNav = document.querySelector(".mobile__nav");
-
+let cross = document.querySelector(".cross");
 mobileTrigger.addEventListener("click", function () {
   mobileTrigger.classList.toggle("mobile__nav-trigger--active");
   mobileNav.classList.toggle("mobile__nav--active");
+  if (cross.classList.contains("active")) {
+    cross.classList.remove("active");
+    cross.classList.add("not-active");
+  } else {
+    cross.classList.remove("not-active");
+
+    cross.classList.add("active");
+  }
 });
 
 let mobileTriggers = document.querySelectorAll(".dropdown__title");
